26317ce50cff43a073092cd63aa807d7dcaa5499
Jan Valo <jan.valo@gmail.com>
2026-02-11 00:41:06 +0100

    Remove build artifacts from Git

--- dist/_worker.js/chunks/_astro_asset-imports_Dno0vHp6.mjs
+++
@@ -1,4 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-const _astro_assetImports = new Map();
-
-export { _astro_assetImports as default };
--- dist/_worker.js/chunks/_astro_data-layer-content_tqJQiZwR.mjs
+++
@@ -1,4 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-const _astro_dataLayerContent = new Map();
-
-export { _astro_dataLayerContent as default };
